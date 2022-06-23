5a843a75a9828a51dbdb39730d4c6108c071517e
edznan <edznan.jusufovic@outlook.com>
2022-06-24 01:45:20 +0200

    initial UI and form setup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,41 @@
-import { Component, VERSION } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import {
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  Validators,
+} from '@angular/forms';
 
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent  {
-  name = 'Angular ' + VERSION.major;
+export class AppComponent implements OnInit {
+  form: FormGroup = new FormGroup({});
+  error = '';
+  hint = '';
+  toastMessage = '';
+  toastLabel = '';
+
+  constructor(private fb: FormBuilder) {}
+
+  ngOnInit(): void {
+    this.form = this.fb.group({
+      name: new FormControl('', [Validators.required]),
+      age: new FormControl('', [Validators.required]),
+      email: new FormControl('', [Validators.required, Validators.email]),
+      password: new FormControl('', [
+        Validators.required,
+        Validators.minLength(8),
+      ]),
+      phone: new FormControl('', [Validators.required]),
+      graduationDate: new FormControl(''),
+      note: new FormControl(''),
+    });
+  }
+
+  onSubmit(form: FormGroup) {
+    console.log(form.value);
+  }
 }
