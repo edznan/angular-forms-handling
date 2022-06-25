f637a20548964e1dfa0c55363b47f56ffbce8782
edznan <edznan.jusufovic@outlook.com>
2022-06-25 02:42:32 +0200

    form validation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import {
   FormBuilder,
   FormControl,
@@ -12,18 +12,19 @@ import {
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent implements OnInit {
+  @ViewChild('toastElement', { static: true })
+  toastEl!: ElementRef;
+
   form: FormGroup = new FormGroup({});
-  error = '';
-  hint = '';
-  toastMessage = '';
-  toastLabel = '';
+
+  response: any = {};
 
   constructor(private fb: FormBuilder) {}
 
   ngOnInit(): void {
     this.form = this.fb.group({
-      name: new FormControl('', [Validators.required]),
-      age: new FormControl('', [Validators.required]),
+      name: new FormControl('', [Validators.required, Validators.minLength(2)]),
+      age: new FormControl('', [Validators.required, Validators.min(18)]),
       email: new FormControl('', [Validators.required, Validators.email]),
       password: new FormControl('', [
         Validators.required,
@@ -36,6 +37,54 @@ export class AppComponent implements OnInit {
   }
 
   onSubmit(form: FormGroup) {
-    console.log(form.value);
+    if (form.valid) {
+      this.response = {
+        type: 'success',
+        message: 'From filled in properly',
+      };
+    } else {
+      this.response = {
+        type: 'error',
+      };
+      if (!form.controls['name'].valid) {
+        if (form.controls['name'].errors.required) {
+          this.response.message = 'Name is required';
+        } else if (form.controls['name'].errors.minLength) {
+          this.response.message = 'Name needs to contain at least 2 characters';
+        }
+        this.response.source = 'name';
+      }
+      if (!form.controls['age'].valid) {
+        if (form.controls['age'].errors.required) {
+          this.response.message = 'Age is required';
+        } else if (form.controls['age'].errors.min) {
+          this.response.message = 'You are too young';
+        }
+        this.response.source = 'age';
+      }
+      if (!form.controls['email'].valid) {
+        if (form.controls['email'].errors.required) {
+          this.response.message = 'Email is required';
+        } else if (form.controls['email'].errors.email) {
+          this.response.message = 'Please enter a correct email';
+        }
+        this.response.source = 'email';
+      }
+      if (!form.controls['password'].valid) {
+        if (form.controls['password'].errors.required) {
+          this.response.message = 'Password is required';
+        } else if (form.controls['age'].errors.minLength) {
+          this.response.message =
+            'Password has to be at least 8 characters long';
+        }
+        this.response.source = 'password';
+      }
+      if (!form.controls['phone'].valid) {
+        if (form.controls['phone'].errors.required) {
+          this.response.message = 'Phone is required';
+        }
+        this.response.source = 'phone';
+      }
+    }
   }
 }
